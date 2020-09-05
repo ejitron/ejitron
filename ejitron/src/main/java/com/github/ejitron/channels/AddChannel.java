9a16817ef99f2c0680dfaeaf505078e0acefd113
condolent <tacco_9@hotmail.com>
2020-09-05 14:59:44 +0200

    Added welcome message

--- ejitron/src/main/java/com/github/ejitron/channels/AddChannel.java
+++ ejitron/src/main/java/com/github/ejitron/channels/AddChannel.java
@@ -3,6 +3,7 @@ package com.github.ejitron.channels;
 import java.util.List;
 
 import com.github.ejitron.Bot;
+import com.github.ejitron.chat.Chat;
 import com.github.ejitron.sql.channels.Channel;
 
 public class AddChannel {
@@ -13,8 +14,15 @@ public class AddChannel {
 		List<String> savedChannels = channels.getAddedChannels(); // List of added channels
 		
 		savedChannels.forEach(channel -> {
-			if(!currentChannels.contains(channel)) // If we're not in a registered channel
+			if(!currentChannels.contains(channel)) { // If we're not in a registered channel
 				joinChannel(channel);
+
+				Chat chat = new Chat();
+				chat.sendMessage(channel, "Hello, " + channel + "! I'm super excited to be here. "
+						+ "First things first; in order for me to function properly I need moderator permissions. "
+						+ "You can grant me this by typing /mod ejitron "
+						+ "And once that's done you can head over to ejitron.tv to finish setting me up!");
+			}
 		});
 	}
 	
