1745e922a3f39ee2beabcd69df9afdf57562658d
condolent <tacco_9@hotmail.com>
2020-09-07 17:31:25 +0200

    Refactor watchtimer

--- ejitron/src/main/java/com/github/ejitron/chat/user/WatchTimer.java
+++ ejitron/src/main/java/com/github/ejitron/chat/user/WatchTimer.java
@@ -10,52 +10,61 @@ import com.github.ejitron.sql.user.WatchTimeListing;
 import com.github.twitch4j.tmi.domain.Chatters;
 
 public class WatchTimer {
+	
+	private List<WatchTime> watchList = Bot.watchTimeList;
 
 	public WatchTimer() {
 		Stream stream = new Stream();
-		Setting setting = new Setting();
 		Channel channel = new Channel();
 		WatchTimeListing watchTimeListing = new WatchTimeListing();
 		List<String> joinedChannels = channel.getAddedChannels();
-		List<WatchTime> watchList = Bot.watchTimeList;
 		
 		joinedChannels.forEach(ch -> {
 			if(stream.getStream(ch) == null) // If the stream is offline, don't count!
 				return;
 			
-			if(setting.getChannelSetting(ch, "watchtime") == 1) { // Make sure the watchtime setting is enabled
-				Chatters chatList = Bot.twitchClient.getMessagingInterface().getChatters(ch).execute();
-				List<String> allViewers = chatList.getAllViewers();
-				
-				if(allViewers.size() < 1) // Skip if there's nobody watching
-					return;
-				
-				allViewers.forEach(viewer -> {
-					if(watchTimeListing.getKnownLurkers().contains(viewer) || viewer.equalsIgnoreCase(ch)) // Don't record known lurkers, or the channel broadcaster
-						return;
-					
-					boolean userDone = false; // Used to check if a viewer has been processed
-						
-					for(int i = 0; i < watchList.size(); i++) { // Check if the viewer is in the local list already
-						WatchTime current = watchList.get(i);
-						
-						if(current.getChannel().equalsIgnoreCase(ch) && current.getUser().equalsIgnoreCase(viewer)) {
-							watchList.set(i, new WatchTime(ch, viewer, current.getMinutes() + 1)); // increment the watched minutes
-							userDone = true;
-							break;
-						}
-					}
-					
-					if(!userDone) // User is not in the local list yet
-						watchList.add(new WatchTime(ch, viewer, 1)); // Create a new entry for viewer
-					
-					return;
-				});
-			}
+			updateLocalList(ch, watchTimeListing); // Update our local list
 		});
 		
 		Bot.watchTimeList = watchList; // Update the watch list to our modified one
 		
+		updateStoredList(watchTimeListing); // Update the database list
+	}
+	
+	public void updateLocalList(String ch, WatchTimeListing watchTimeListing) {
+		Setting setting = new Setting();
+		if(setting.getChannelSetting(ch, "watchtime") == 1) { // Make sure the watchtime setting is enabled
+			Chatters chatList = Bot.twitchClient.getMessagingInterface().getChatters(ch).execute();
+			List<String> allViewers = chatList.getAllViewers();
+			
+			if(allViewers.size() < 1) // Skip if there's nobody watching
+				return;
+			
+			allViewers.forEach(viewer -> {
+				if(watchTimeListing.getKnownLurkers().contains(viewer) || viewer.equalsIgnoreCase(ch)) // Don't record known lurkers, or the channel broadcaster
+					return;
+				
+				boolean userDone = false; // Used to check if a viewer has been processed
+					
+				for(int i = 0; i < watchList.size(); i++) { // Check if the viewer is in the local list already
+					WatchTime current = watchList.get(i);
+					
+					if(current.getChannel().equalsIgnoreCase(ch) && current.getUser().equalsIgnoreCase(viewer)) {
+						watchList.set(i, new WatchTime(ch, viewer, current.getMinutes() + 1)); // increment the watched minutes
+						userDone = true;
+						break;
+					}
+				}
+				
+				if(!userDone) // User is not in the local list yet
+					watchList.add(new WatchTime(ch, viewer, 1)); // Create a new entry for viewer
+				
+				return;
+			});
+		}
+	}
+	
+	public void updateStoredList(WatchTimeListing watchTimeListing) {
 		List<WatchTime> savedWatchTime = watchTimeListing.getSavedWatchTime();
 		
 		watchList.forEach(cl -> {
