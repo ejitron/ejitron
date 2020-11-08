f281d5a3215e4270f88f35dfb0b974ad4cc72bc5
condolent <tacco_9@hotmail.com>
2020-11-08 12:47:45 +0100

    Fixed websocket issue

--- ejitron/src/main/java/com/github/ejitron/Launcher.java
+++ ejitron/src/main/java/com/github/ejitron/Launcher.java
@@ -8,14 +8,19 @@ public final class Launcher {
 	
 	private Launcher() {}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Bot bot = new Bot();
-		WebSocket webSocket = new WebSocket();
-		
-		// Launch the websocket server
-		webSocket.start(6967);
 		
 		// Load configuration and join channels
 		bot.start();
+		
+		// Launch the websocket server
+		WebSocket webSocket = new WebSocket();
+		try {
+			webSocket.start(6967);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- ejitron/src/main/java/com/github/ejitron/socket/SocketHandler.java
+++ ejitron/src/main/java/com/github/ejitron/socket/SocketHandler.java
@@ -4,6 +4,10 @@ public class SocketHandler {
 	
 	public void onMessageReceived(String message) {
 		String args[] = message.split("\\s+");
+		
+		if(args.length <= 1) // We need at least 2 args to actually do anything here
+			return;
+		
 		String setting = args[0];
 		String channel = args[1];
 		
