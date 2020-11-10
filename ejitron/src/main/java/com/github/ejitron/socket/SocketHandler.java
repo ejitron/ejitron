0ecacb19aef8f124983b5e8b5d93efdd83f16869
condolent <tacco_9@hotmail.com>
2020-11-10 21:10:30 +0100

    Join chat once button is pressed in dashboard

--- ejitron/src/main/java/com/github/ejitron/socket/SocketHandler.java
+++ ejitron/src/main/java/com/github/ejitron/socket/SocketHandler.java
@@ -1,5 +1,12 @@
 package com.github.ejitron.socket;
 
+import com.github.ejitron.Bot;
+import com.github.ejitron.chat.Chat;
+import com.github.ejitron.sql.channels.Channel;
+import com.github.twitch4j.chat.TwitchChat;
+
+import java.util.Map;
+
 public class SocketHandler {
 	
 	public void onMessageReceived(String message) {
@@ -10,12 +17,31 @@ public class SocketHandler {
 		
 		String setting = args[0];
 		String channel = args[1];
-		
-		/*
-		 * Example message:
-		 * cmd rlhypr
-		 * Will refresh the local list of commands for the channel of rlHypr
-		 */
+
+		// Someone pressed "Join chat" in the dashboard
+		if(setting.equalsIgnoreCase("join")) {
+			Channel channels = new Channel();
+			Chat chat = new Chat();
+			Map<String, Integer> registeredChannels = channels.getAddedChannels();
+			TwitchChat chatClient = Bot.twitchClient.getChat();
+
+			if(!registeredChannels.containsKey(channel.toLowerCase())) // Make sure that channel is registered
+				return;
+
+			if(chatClient.isChannelJoined(channel)) // Ignore if we're already in chat
+				return;
+
+			chatClient.joinChannel(channel);
+
+			if(registeredChannels.get(channel) == 1) { // Let's check if this channel is completely new
+				chat.sendMessage(channel, "Hello, " + channel + "! I'm super excited to be here. "
+						+ "First things first; in order for me to function properly I need moderator permissions. "
+						+ "You can grant me this by typing /mod ejitron "
+						+ "If you want me to check the status of the setup, type !eji check");
+			} else {
+				chat.sendMessage(channel, "/me just entered the building");
+			}
+		}
 	}
 	
 }
