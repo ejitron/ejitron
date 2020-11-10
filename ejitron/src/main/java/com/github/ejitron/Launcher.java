c310eeec4d8dca3a508125c788c53378e0992da8
condolent <tacco_9@hotmail.com>
2020-11-11 00:28:55 +0100

    Cleanup

--- ejitron/src/main/java/com/github/ejitron/Launcher.java
+++ ejitron/src/main/java/com/github/ejitron/Launcher.java
@@ -6,20 +6,19 @@ import com.github.ejitron.socket.WebSocket;
 
 public final class Launcher {
 	
-	private Launcher() {}
+//	private Launcher() {}
 	
 	public static void main(String[] args) {
-		Bot bot = new Bot();
-		
 		// Load configuration and join channels
+		Bot bot = new Bot();
 		bot.start();
-		
+
 		// Launch the websocket server
 		WebSocket webSocket = new WebSocket();
 		try {
+			System.out.println("Websocket launching...");
 			webSocket.start(6967);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
--- ejitron/src/main/java/com/github/ejitron/socket/WebSocket.java
+++ ejitron/src/main/java/com/github/ejitron/socket/WebSocket.java
@@ -1,14 +1,17 @@
 package com.github.ejitron.socket;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class WebSocket {
 	
 	/**
 	 * Starts the actual websocket server.
 	 * @param port Which port to have the server to listen to.
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public void start(int port) throws IOException {
 		String clientSentence;
